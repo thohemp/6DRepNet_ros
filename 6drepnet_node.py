7610d6d1cb7f609564ef2914ad29bb15faa8e825
thohemp <thorstenhempel@web.de>
2022-03-08 14:53:46 +0100

    Update

--- 6drepnet_node.py
+++ 6drepnet_node.py
@@ -2,17 +2,14 @@
 
 from model import SixDRepNet
 import math
-import re
-from matplotlib import pyplot as plt
 import sys
 import os
 import argparse
 import rospy
+import rospkg
 
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-from numpy.lib.function_base import _quantile_unchecked
 
 import torch
 import torch.nn as nn
@@ -22,15 +19,15 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torch.nn.functional as F
 import utils
-import matplotlib
 from PIL import Image
 
 from sensor_msgs.msg import Image as Image_msg
+from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Header
 
 import time
-matplotlib.use('TkAgg')
 from face_detection import RetinaFace
+
 
 class SixDRepNet_Node:
     def __init__(self):
@@ -39,12 +36,14 @@ class SixDRepNet_Node:
         self.gpu = args.gpu_id
         self.cam = args.cam_id
         self.snapshot_path = args.snapshot
+        self.sub_topic = args.image_topic
+        self.detector = RetinaFace(gpu_id=self.gpu) 
 
         # Node cycle rate (in Hz).
         self.loop_rate = rospy.Rate(1)
 
         self.pub = rospy.Publisher('sixdrepnet/image', Image_msg,queue_size=10)
-
+        if self.sub_topic is not '': self.sub = rospy.Subscriber(self.sub_topic, CompressedImage, self.image_callback) 
 
         self.model = SixDRepNet(backbone_name='RepVGG-B1g2',
                             backbone_file='',
@@ -70,10 +69,14 @@ class SixDRepNet_Node:
         # Test the Model
         self.model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
 
-        try:
-            self.run_6drepnet()
-        except rospy.ROSInterruptException:
-            pass
+        if self.sub_topic == '':
+            try:
+                pass#self.run_6drepnet()
+            except rospy.ROSInterruptException:
+                pass
+        else:
+            print(self.sub_topic)
+            rospy.spin()
 
     def parse_args(self):
         """Parse input arguments."""
@@ -88,14 +91,21 @@ class SixDRepNet_Node:
         parser.add_argument('--snapshot',
                             dest='snapshot', help='Name of model snapshot.',
                             default='model/6DRepNet_300W_LP_AFLW2000.pth', type=str)
-        parser.add_argument('--save_viz',
-                            dest='save_viz', help='Save images with pose cube.',
-                            default=False, type=bool)
-
+        parser.add_argument('--image_topic',
+                            dest='image_topic', help='Compressed image topic to subscribe to.',
+                            default='', type=str)
 
 
         args = parser.parse_args()
         return args
+
+    def image_callback(self, data):
+        #img = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
+        img = np.fromstring(data.data, np.uint8)
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+        #cv2.imshow("test", img)
+        #(cv2.waitKey(5)
+        self.inference(img)
 
     def publish_image(self, imgdata):
         h, w, c = imgdata.shape
@@ -110,77 +120,79 @@ class SixDRepNet_Node:
         image_temp.step=w*3
         self.pub.publish(image_temp)
 
-    def run_6drepnet(self):
-
-        detector = RetinaFace(gpu_id=self.gpu) 
+    def run_camera(self):
         cap = cv2.VideoCapture(self.cam)
-
         # Check if the webcam is opened correctly
         if not cap.isOpened():
             raise IOError("Cannot open webcam")
 
+        while not rospy.is_shutdown():
+            ret, frame = cap.read()
 
+            self.inference(frame)
+
+    def inference(self, frame):
         with torch.no_grad():
-            while not rospy.is_shutdown():
-                ret, frame = cap.read()
 
-                faces = detector(frame)
+            faces = self.detector(frame)
 
-                for box, landmarks, score in faces:
+            for box, landmarks, score in faces:
 
-                    # Print the location of each face in this image
-                    if score < .95:
-                        continue
-                    x_min = int(box[0])
-                    y_min = int(box[1])
-                    x_max = int(box[2])
-                    y_max = int(box[3])         
-                    bbox_width = abs(x_max - x_min)
-                    bbox_height = abs(y_max - y_min)
+                # Print the location of each face in this image
+                if score < .95:
+                    continue
+                x_min = int(box[0])
+                y_min = int(box[1])
+                x_max = int(box[2])
+                y_max = int(box[3])         
+                bbox_width = abs(x_max - x_min)
+                bbox_height = abs(y_max - y_min)
 
-                    x_min = max(0,x_min-int(0.2*bbox_height))
-                    y_min = max(0,y_min-int(0.2*bbox_width))
-                    x_max = x_max+int(0.2*bbox_height)
-                    y_max = y_max+int(0.2*bbox_width)
+                x_min = max(0,x_min-int(0.2*bbox_height))
+                y_min = max(0,y_min-int(0.2*bbox_width))
+                x_max = x_max+int(0.2*bbox_height)
+                y_max = y_max+int(0.2*bbox_width)
 
-                    img = frame[y_min:y_max,x_min:x_max]
-                    img = img.convert('RGB')
-                    img = self.transformations(img)
+                img = frame[y_min:y_max,x_min:x_max]
+                img = Image.fromarray(img)
+                img = img.convert('RGB')
+                img = self.transformations(img)
 
-                    img = torch.Tensor(img[None, :]).cuda(self.gpu)
+                img = torch.Tensor(img[None, :]).cuda(self.gpu)
 
 
-                    c = cv2.waitKey(1)
-                    if c == 27:
-                        break
-                        
-                    start = time.time()
-                    R_pred = self.model(img)
-                    end = time.time()
-                    print('Head pose estimation: %2f ms'% ((end - start)*1000.))
-
-                    euler = utils.compute_euler_angles_from_rotation_matrices(
-                        R_pred)*180/np.pi
-                    p_pred_deg = euler[:, 0].cpu()
-                    y_pred_deg = euler[:, 1].cpu()
-                    r_pred_deg = euler[:, 2].cpu()
-
+                c = cv2.waitKey(1)
+                if c == 27:
+                    break
                     
-                    #utils.draw_axis(frame, y_pred_deg, p_pred_deg, r_pred_deg, left+int(.5*(right-left)), top, size=100)
-                    utils.plot_pose_cube(frame,  y_pred_deg, p_pred_deg, r_pred_deg, x_min + int(.5*(x_max-x_min)), y_min + int(.5*(y_max-y_min)), size = bbox_width)
-                    
+                start = time.time()
+                R_pred = self.model(img)
+                end = time.time()
+                print('Head pose estimation: %2f ms'% ((end - start)*1000.))
+
+                euler = utils.compute_euler_angles_from_rotation_matrices(
+                    R_pred)*180/np.pi
+                p_pred_deg = euler[:, 0].cpu()
+                y_pred_deg = euler[:, 1].cpu()
+                r_pred_deg = euler[:, 2].cpu()
+
                 
-                #cv2.imshow("Demo", frame)
-                #cv2.waitKey(5)
+                #utils.draw_axis(frame, y_pred_deg, p_pred_deg, r_pred_deg, left+int(.5*(right-left)), top, size=100)
+                utils.plot_pose_cube(frame,  y_pred_deg, p_pred_deg, r_pred_deg, x_min + int(.5*(x_max-x_min)), y_min + int(.5*(y_max-y_min)), size = bbox_width)
+                
+            
+            cv2.imshow("Demo", frame)
+            cv2.waitKey(5)
 
-                # Publish image 
-                self.publish_image(frame)
-               # self.loop_rate.sleep()
+            # Publish image 
+            self.publish_image(frame)
+            # self.loop_rate.sleep()
+
+  
 
 if __name__ == '__main__':
     rospy.init_node('sixdrepnet_head_pose', anonymous=True)
     sixdrepnet_node = SixDRepNet_Node()
-    sixdrepnet_node.start()
 
 
 
