631c90ae3c9d4bb01403e73ba9c34d2dcb0a4ab2
thohemp <thorstenhempel@web.de>
2022-04-25 15:40:48 +0200

    Update subscriber script

--- 6drepnet_sub_rot.py
+++ 6drepnet_sub_rot.py
@@ -8,9 +8,66 @@ import rospkg
 from geometry_msgs.msg import Quaternion, PoseStamped, Pose
 import message_filters
 from sensor_msgs.msg import CompressedImage, CameraInfo, Image
-import utils
 
 import numpy as np, cv2
+import math
+
+def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size = 100):
+
+    pitch = pitch * np.pi / 180
+    yaw = -(yaw * np.pi / 180)
+    roll = roll * np.pi / 180
+
+    if tdx != None and tdy != None:
+        tdx = tdx
+        tdy = tdy
+    else:
+        height, width = img.shape[:2]
+        tdx = width / 2
+        tdy = height / 2
+
+    # X-Axis pointing to right. drawn in red
+    x1 = size * (math.cos(yaw) * math.cos(roll)) + tdx
+    y1 = size * (math.cos(pitch) * math.sin(roll) + math.cos(roll) * math.sin(pitch) * math.sin(yaw)) + tdy
+
+    # Y-Axis | drawn in green
+    #        v
+    x2 = size * (-math.cos(yaw) * math.sin(roll)) + tdx
+    y2 = size * (math.cos(pitch) * math.cos(roll) - math.sin(pitch) * math.sin(yaw) * math.sin(roll)) + tdy
+
+    # Z-Axis (out of the screen) drawn in blue
+    x3 = size * (math.sin(yaw)) + tdx
+    y3 = size * (-math.cos(yaw) * math.sin(pitch)) + tdy
+
+    cv2.line(img, (int(tdx), int(tdy)), (int(x1),int(y1)),(0,0,255),4)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x2),int(y2)),(0,255,0),4)
+    cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(255,0,0),4)
+
+    return img
+
+def euler_from_quaternion(x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+
+        return roll_x, pitch_y, yaw_z # in radians
+
+
 
 class SixDRepNet_Processor_Node:
 
@@ -20,11 +77,11 @@ class SixDRepNet_Processor_Node:
         self.sub_topic = args.image_topic
         self.c = args.c
 
-        self.pub_img_compr =  rospy.Publisher('sixdrepnet/processed/image_raw/compressed', CompressedImage, queue_size=10)
+        self.pub_img_compr =  rospy.Publisher('sixdrepnet/process/image_raw/compressed', CompressedImage, queue_size=10)
         self.pub_img = rospy.Publisher('sixdrepnet/processed/image_raw/', Image, queue_size=10)
 
-        rot_sub = message_filters.Subscriber('sixdrepnet/rotation', PoseStamped) 
-        image_sub = message_filters.Subscriber('/usb_cam/image_raw', Image)
+        rot_sub = message_filters.Subscriber('sixdrepnet/rotation', PoseStamped)
+        image_sub = message_filters.Subscriber('/head_front_camera/color/image_raw/compressed', CompressedImage)
         ts = message_filters.ApproximateTimeSynchronizer([image_sub, rot_sub], 10,10)
         ts.registerCallback(self.rot_callback)
 
@@ -50,21 +107,21 @@ class SixDRepNet_Processor_Node:
     def rot_callback(self, img_data, rot_data):
         #print("{} {} ".format(img_data.header.stamp, rot_data.header.stamp))
         # Image
-        #img = np.fromstring(img_data.data, np.uint8)
-        #img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-        img = np.frombuffer(img_data.data, dtype=np.uint8).reshape(img_data.height, img_data.width, -1)
+        img = np.fromstring(img_data.data, np.uint8)
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+        #img = np.frombuffer(img_data.data, dtype=np.uint8).reshape(img_data.height, img_data.width, -1)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # Rotation
         pose = rot_data.pose
         rot = pose.orientation
-        roll, pitch, yaw = utils.euler_from_quaternion(rot.x, rot.y, rot.z, rot.w)
+        roll, pitch, yaw = euler_from_quaternion(rot.x, rot.y, rot.z, rot.w)
 
-        utils.draw_axis(img, yaw*180/np.pi, pitch*180/np.pi, roll*180/np.pi, 50, 30, size=100)
+        draw_axis(img, yaw*180/np.pi, pitch*180/np.pi, roll*180/np.pi, 50, 30, size=100)
 
         self.publish_image_compressed(img)
         self.publish_image(img)
-   
-    
+
+
     def publish_image(self, imgdata):
         h, w, c = imgdata.shape
         image_temp=Image()
@@ -92,4 +149,3 @@ class SixDRepNet_Processor_Node:
 if __name__ == '__main__':
     rospy.init_node('sixdrepnet_processor', anonymous=True)
     sixdrepnet_node = SixDRepNet_Processor_Node()
-
